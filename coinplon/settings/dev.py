e59b42e27427fd6eb878dd6c7b1d84e6965505cd
EmadSimplon <e.a.darwich@gmail.com>
2022-10-10 15:20:59 +0200

    initals commit

---
+++ coinplon/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-v79m*=5*!t#$01=d+w)&t*z^muj+7f_jr+lcp^+idf%_@v1gsz"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
